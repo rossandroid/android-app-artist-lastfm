6ae9095ec1ce2fa42bc250cd908f715b248048a2
Rossella Morgante <morgante.rossella@gmail.com>
2019-04-08 21:28:28 +0100

    recycleAdapter and itemRow WIP

---
+++ app/src/main/java/com/lfm/rossellamorgante/lfm/Adapter/LFMRecAdapter.java
@@ -0,0 +1,60 @@
+package com.lfm.rossellamorgante.lfm.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lfm.rossellamorgante.lfm.Model.Artist;
+import com.lfm.rossellamorgante.lfm.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class LFMRecAdapter extends RecyclerView.Adapter <LFMRecAdapter.ViewHolder> {
+
+    private List<Artist> mArtists;
+    private LayoutInflater mInflater;
+
+    public LFMRecAdapter(List<Artist> data) {
+        this.mArtists = data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View item = inflater.inflate(R.layout.list_item, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(item);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        Artist a = mArtists.get(position);
+        holder.artistName.setText(a.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.mArtists.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView artistName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            artistName = (TextView) itemView.findViewById(R.id.artistName);
+        }
+    }
+}
--- app/src/main/java/com/lfm/rossellamorgante/lfm/MainActivity.java
+++ app/src/main/java/com/lfm/rossellamorgante/lfm/MainActivity.java
@@ -4,31 +4,51 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.lfm.rossellamorgante.lfm.Adapter.LFMRecAdapter;
 import com.lfm.rossellamorgante.lfm.Model.Artist;
 import com.lfm.rossellamorgante.lfm.ViewModel.LFMViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     LFMViewModel viewModel;
+    private RecyclerView recyclerView;
+    private LFMRecAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        viewModel = ViewModelProviders.of(this).get(LFMViewModel.class);
+        // Recycle View
+        recyclerView = (RecyclerView) findViewById(R.id.artist_list);
+        recyclerView.setHasFixedSize(true);
+
+        List<Artist> l = new ArrayList<Artist>();
+        Artist a = new Artist();
+        a.setName("AAAAA");
+        l.add(a);
+
+        mAdapter = new LFMRecAdapter(l);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        /*viewModel = ViewModelProviders.of(this).get(LFMViewModel.class);
         viewModel.getArtists().observe(this,
                 new Observer<List<Artist>>() {
                     @Override
                     public void onChanged(@Nullable final List<Artist> _arstist) {
-                        // adapter notify
+                        mAdapter.notifyDataSetChanged();
                     }
                 }
-        );
-    }
+            );*/
+        }
 }
