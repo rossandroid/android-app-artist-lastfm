895681206a62fe72b8cf3868cf9f7c0d71d9710f
Rossella Morgante <morgante.rossella@gmail.com>
2019-04-09 20:12:57 +0100

    Serach bar - MainActivity (list) completed

--- app/src/main/java/com/lfm/rossellamorgante/lfm/MainActivity.java
+++ app/src/main/java/com/lfm/rossellamorgante/lfm/MainActivity.java
@@ -2,12 +2,17 @@ package com.lfm.rossellamorgante.lfm;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.lfm.rossellamorgante.lfm.Adapter.LFMRecAdapter;
 import com.lfm.rossellamorgante.lfm.Model.Artist;
@@ -43,14 +48,10 @@ public class MainActivity extends AppCompatActivity  {
                 new Observer<List<Artist>>() {
                     @Override
                     public void onChanged(@Nullable final List<Artist> _arstist) {
-                        updateRecicleView(_arstist);
+                        if(_arstist!=null)updateRecicleView(_arstist);
                     }
                 }
             );
-
-
-        viewModel.search("cher");
-
 
     }
 
@@ -58,6 +59,33 @@ public class MainActivity extends AppCompatActivity  {
         list.addAll(_arstist);
         mAdapter.notifyDataSetChanged();
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searchmenu, menu);
+        MenuItem searchViewItem = menu.findItem(R.id.app_bar_search);
+
+        final SearchView searchView = (SearchView) searchViewItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchView.clearFocus();
+                //setSpinner(true,false,"Searching...");
+                viewModel.search(query);
+                return false;
+            }
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                list.clear();
+                mAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 
 }
